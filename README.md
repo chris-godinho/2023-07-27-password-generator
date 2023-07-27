9be1fa6f1cd83779c1936cb1c448c54c875d2d53
kristbg <kristbg@gmail.com>
2023-07-27 16:35:14 -0400

    Application can now be used repeatedly

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# cbg-mod03-password-generator
-A simple password generating web application.
+# Module 3 Challenge - Password Generator
+
+## Description
+
+A simple web-based password generator with customized options for length and types of characters.
+
+![A render of the finished page](/assets/images/page_render.jpg)
+
+## Features
+
+   - Application checks for valid password length, as well as four possible character pools;
+   - Application can be used repeatedly without the need to reload the page.
+
+## Installation
+
+No installation is necessary for this application. A live page can be accessed at <https://kristbg.github.io/cbg-mod03-password-generator/>.
+
+## How can I Contribute?
+
+This is a one-off project, so I'm afraid you cannot. Feel free to clone this repo if you want, but please make sure to credit the original creators.
+
+## Credits
+
+This application was created by the University of Toronto's School of Continuing Studies, with additions (JavaScript) by Chris Godinho.
+
+Part of the research for this project was done through the website Stack Overflow (<https://stackoverflow.com/>), and as such small sections of code were adapted from examples offered in that site.
+
+## License
+
+This project is licensed under the [MIT](/LICENSE) License.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,36 @@
-// Assignment code here
+// Assign all four possible character pools
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const special = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+// Initialize variables globally to allow for repeated use
+var password;
 var characterPool = [];
 var passLength;
+var passwordText;
+var generatedPassword;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Check for valid password length
 function checkLength() {
+  passLength = 0;
+  characterPool = [];
   while (!Number.isInteger(passLength) || passLength < 8 || passLength > 128) {
     passLength = prompt("Enter the number of characters to include in your password (between 8 and 128):");
+    // If user clicks "cancel"
     if (passLength == null) {
       return false;
     } else {
+      // Check for valid length
       passLength = Number(passLength);
       if (!Number.isInteger(passLength)) {
-        alert("Please enter a whole number (no decimals!) between 8 and 128.")
-      } else if (passLength < 8 || passLength > 128) {
-        alert("Please choose a number between 8 and 128.")
+        alert("Please enter a valid whole number between 8 and 128.");
+      } else if (passLength < 8) {
+        alert("Too short! Please choose a number between 8 and 128.");
+      } else if (passLength > 128) {
+        alert("Too long! Please choose a number between 8 and 128.");
       } else {
         return true;
       }
@@ -30,7 +40,7 @@ function checkLength() {
 
 // Generate password based on prompts
 function generatePassword() {
-  var generatedPassword = "";
+  generatedPassword = "";
   if (checkLength()) {
     if (confirm("Would you like lowercase letters in your password?") === true) {
       characterPool = characterPool.concat(lowercase);
@@ -44,9 +54,11 @@ function generatePassword() {
     if (confirm("Would you like special characters in your password?") === true) {
       characterPool = characterPool.concat(special);
     }
+    // If user doesn't pick any possible characters
     if (characterPool.length === 0) {
       alert("Sorry, we need at least one group of characters to generate a password!");
     } else {
+      // Generate password
       for(var i = 0; i < passLength; i++) {
         generatedPassword = generatedPassword + characterPool[Math.floor(Math.random() * characterPool.length)];
       }
@@ -57,10 +69,12 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText = document.querySelector("#password");
+  password = generatePassword();
+  // Avoid showing "undefined" in the password field
+  if (password !== undefined) {
+    passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
